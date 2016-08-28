4f4036734fa8fd88ed26022d546d1f9d2bdecf5b
blueplanet <erguolinge@gmail.com>
2016-08-29 06:40:45 +0900

    Add api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'comments/index'
   root 'comments#index'
 
+  namespace :api, format: :json do
+    namespace :v1 do
+      resources :comments
+    end
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
